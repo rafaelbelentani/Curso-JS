d7fb98f65e2b4048c6d69347b38251d8b6ecb26f
Rafael Luiz Belentani <135762922+rafaelbelentani@users.noreply.github.com>
2024-10-05 13:46:11 -0300

    Add files via upload

---
+++ Desafio.js
@@ -0,0 +1,8 @@
+let n1 = 8;
+let n2 = 7;
+let n3 = 3;
+let n4 = 5;
+
+let media = (n1 + n2 + n3 + n4) / 4 ;
+
+console.log(`A média do aluno é ${media}`)
---
+++ index.js
@@ -0,0 +1,49 @@
+let nome = "Rafael";
+let sobrenome ="Qualquer";
+let idade = 25;
+
+let NomeCompleto = nome + " " + sobrenome
+
+console.log(NomeCompleto);
+
+let numero1 = 10;
+let numero2 = 10;
+
+let numeroDecimal = 3.14;
+
+let booleano = true;
+
+let nulo = null;
+
+let indefeinido = undefined;
+
+console.log("Olá meu nome é " + nome + " eu tenho " + idade + " anos" );
+
+let frase_interopolada = `Olá, meu nome é ${nome} e tenho ${idade} anos`;
+
+console.log(frase_interopolada);
+
+
+const numero = 8;
+let multiplicador = 10;
+console.log(`${numero} x ${multiplicador} = ${numero*multiplicador}`)
+multiplicador--;
+console.log(`${numero} x ${multiplicador} = ${numero*multiplicador}`)
+--multiplicador;
+console.log(`${numero} x ${multiplicador} = ${numero*multiplicador}`)
+
+console.log(++multiplicador)
+console.log(++multiplicador)
+
+const numero3 = 8;
+let multiplicador2 = 1;
+console.log(`${numero3} x ${multiplicador2} = ${numero*multiplicador2++}`)
+console.log(`${numero3} x ${multiplicador2} = ${numero*multiplicador2++}`)
+console.log(`${numero3} x ${multiplicador2} = ${numero*multiplicador2++}`)
+console.log(`${numero3} x ${multiplicador2} = ${numero*multiplicador2++}`)
+console.log(`${numero3} x ${multiplicador2} = ${numero*multiplicador2++}`)
+console.log(`${numero3} x ${multiplicador2} = ${numero*multiplicador2++}`)
+console.log(`${numero3} x ${multiplicador2} = ${numero*multiplicador2++}`)
+console.log(`${numero3} x ${multiplicador2} = ${numero*multiplicador2++}`)
+console.log(`${numero3} x ${multiplicador2} = ${numero*multiplicador2++}`)
+console.log(`${numero3} x ${multiplicador2} = ${numero*multiplicador2++}`)
